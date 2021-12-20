005fe803c00865046470a17d6be9d633737ac24f
tharusha555 <87853645+tharusha555@users.noreply.github.com>
2021-12-20 23:41:48 +0530

    Add files via upload

---
+++ API/pages/index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+
+
+
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("content-type: application/json");
+
+
+
+echo json_encode(array('data' => 'temproray pages not working defolate pages author imator to access'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
